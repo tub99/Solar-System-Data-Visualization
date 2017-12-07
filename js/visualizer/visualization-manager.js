050b41e806c10468c4358abc3493c8bfe17ab2ad
soumya.ghosh <soumya.ghosh@learningmate.com>
2017-12-07 20:50:33 +0530

    satellites added

--- js/visualizer/visualization-manager.js
+++ js/visualizer/visualization-manager.js
@@ -2,6 +2,7 @@ var VisualizationManager = function () {};
 
 VisualizationManager.prototype.createSolarSystem = function(paper){
     var viz = new Visualizer();
-    viz.setClientInput(40, 5);
+    viz.setClientInput(20, 5);
     viz.renderSolarSystem(paper);
+    //viz.searchPlanet('planet_3');
 }
--- js/visualizer/visualizer.js
+++ js/visualizer/visualizer.js
@@ -1,53 +1,103 @@
 function Visualizer() {
     var paperInstance,
-        equidistantFactor = 60,
+        equidistantFactor = 30,
         clientInput = {
             channelsNo: 0,
             planetsNo: 0
         },
         graphInputs = {
             canvasX: 400,
-            canvasY: 300,
-            planetRadius: 0,
-            sunRadius:0,
-            channel: ''
+            canvasY: 350,
+            planetRadius: 2,
+            sunRadius: 30,
+            channel: '',
+            satteliteRadius: 7
         },
         planetsData = [],
         channelsData = [],
+        satellitesData = [],
 
-        generatePlanetInfo = function (radius, theta) {
+        generatePlanetInfo = function (radius, theta, planetNo, lev) {
+            var entity = (lev !== clientInput.planetsNo) ? 'planet_' : 'satellite_'
             return {
                 X: graphInputs.canvasX + (radius * Math.cos(theta)),
                 Y: graphInputs.canvasY + (radius * Math.sin(theta)),
-                label: '',
+                label: entity + planetNo,
+                level: lev
             };
 
-        }
-    computeSystemPosition = function () {
+        },
+        computeSatelliteAxisPosition = function () {
+            var rad = graphInputs.satteliteRadius;
+            for (var satellite of satellitesData) {
+
+                var x = satellite.X,
+                    y = satellite.Y,
+                    theta = 45 * (Math.PI / 180),
+                    factor = 90 * (Math.PI / 180);
+                // 4 sattelitse at 90 degree separate from each other
+                // 1st one at 45 , 2nd = 45+90 , 3rd = 45+90+90, 4th= 45+270
+                satellite.axis = {
+                    first: {
+                        X: x + rad * Math.cos(theta),
+                        Y: y + rad * Math.sin(theta)
+                    },
+                    second: {
+                        X: x + rad * Math.cos(theta + factor),
+                        Y: y + rad * Math.sin(theta + factor)
+                    },
+                    third: {
+                        X: x + rad * Math.cos(theta + 2 * factor),
+                        Y: y + rad * Math.sin(theta + 2 * factor)
+                    },
+                    fourth: {
+                        X: x + rad * Math.cos(theta + 3 * factor),
+                        Y: y + rad * Math.sin(theta + 3 * factor)
+                    }
+                };
+
+                console.log(satellite);
+
+            }
+
+        },
+        computeSystemPosition = function () {
             var separationangle = 360 / clientInput.channelsNo;
             // parametric position RCosTheta, RSinTheta
-            var totalRad = graphInputs.totalRadius = equidistantFactor * clientInput.planetsNo;
+            var totalRad = graphInputs.totalRadius = equidistantFactor * clientInput.planetsNo,
+                cnt = 0;
             for (var i = 1; i <= clientInput.planetsNo; i++) {
-                for (var j = 1; j <=clientInput.channelsNo; j++) {
+                for (var j = 1; j <= clientInput.channelsNo; j++) {
+                    cnt++;
                     var rad = equidistantFactor * (i),
-                        theta = (separationangle * (j))*(Math.PI/180),
-                        planetInfo = generatePlanetInfo(rad, theta);
+                        theta = (separationangle * (j)) * (Math.PI / 180),
+                        planetInfo = generatePlanetInfo(rad, theta, cnt, i);
                     planetsData.push(planetInfo);
-                    if (i === clientInput.planetsNo) channelsData.push(planetInfo);
+                    // All satellite data
+                    if (i === clientInput.planetsNo) {
+                        // set count for satellite
+                        cnt = 0;
+                        //set the channels extremeties
+                        channelsData.push(planetInfo);
+                        // set the satellites
+                        satellitesData.push(planetInfo);
+                    }
 
                 }
             }
-            console.log(planetsData);
+            computeSatelliteAxisPosition();
+            //console.log(planetsData);
 
 
         },
         renderPlanets = function () {
-            for (let plannet of planetsData) {
-                var circle = paperInstance.circle(plannet.X, plannet.Y, 2);
+            for (let planet of planetsData) {
+                var circle = paperInstance.circle(planet.X, planet.Y, graphInputs.planetRadius);
                 // Sets the fill attribute of the circle to red (#f00)
                 circle.attr({
                     "fill": 'blue'
                 });
+                circle.id = planet.label;
             }
         },
         renderChannels = function () {
@@ -62,13 +112,34 @@ function Visualizer() {
             }
         },
         drawSun = function () {
-            var circle = paperInstance.circle(graphInputs.canvasX, graphInputs.canvasY, 30);
+            var circle = paperInstance.circle(graphInputs.canvasX, graphInputs.canvasY, graphInputs.sunRadius);
             // Sets the fill attribute of the circle to red (#f00)
             circle.attr({
                 "fill": 'white',
                 'stroke-width': 5,
                 'stroke': 'blue'
             });
+        },
+        drawSattelites = function () {
+            for (var satellite of satellitesData) {
+                for (var p in satellite.axis) {
+                    var elem = satellite.axis[p];
+                    //draw axis lines
+                    var line = paperInstance.path(['M', satellite.X, satellite.Y, 'L', elem.X, elem.Y]);
+                    // Sets the fill attribute of the circle to red (#f00)
+                    line.attr({
+                        "stroke": 'blue'
+                    });
+                    var circle = paperInstance.circle(elem.X, elem.Y, 2);
+                    circle.attr({
+                        "fill": 'black'
+                    });
+                }
+            }
+
+
+
+
         }
     this.setClientInput = function (channels, planets) {
         clientInput.channelsNo = channels || 40;
@@ -77,12 +148,21 @@ function Visualizer() {
     this.getClientInput = function () {
         return clientInput;
     }
+    this.searchPlanet = function (planetId) {
+        var planet = paperInstance.getById(planetId);
+        //highlight and zoom the searched planet
+        planet.attr({
+            'fill': 'red',
+            'c': 50
+        })
+    }
     this.renderSolarSystem = function (paper) {
         paperInstance = paper;
         computeSystemPosition();
 
         renderChannels();
         drawSun();
+        drawSattelites();
         renderPlanets();
     }
 }
